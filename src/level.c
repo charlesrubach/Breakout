fc5caca654d8446e0f9ed8513853c60245ca853a
Charles Rubach <charlesrubach@gmail.com>
2026-08-16 18:20:38 -0500

    Fixed issues preventing the bricks from rendering

--- src/level.c
+++ src/level.c
@@ -1,6 +1,9 @@
 #include "level.h"
 #include "cJSON.h"
+#include "config.h"
 #include "game.h"
+#include "brick.h"
+#include "raylib.h"
 #define _POSIX_C_SOURCE 200112L
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +17,9 @@ const char *json_file = "resources/breakout_levels.json";
 
 static char *ReadLevelFile(const char *filename);
 static int GetLevel(Game *game, const char *const breakout_levels);
+static Color HexColorStringToColor(char *hex_str);
 
-void LoadLevel(Game *game)
+void LevelLoad(Game *game)
 {
   // Load breakout_levels.json
 
@@ -27,9 +31,27 @@ void LoadLevel(Game *game)
     return;
   }
 
+  game->level->brick_width = SCREEN_WIDTH / BRICK_COLUMNS;
+  game->level->brick_height = SCREEN_HEIGHT / 2 / BRICK_ROWS;
+
   GetLevel(game, buffer);
 
   free(buffer);
+}
+
+void LevelUpdate(Level *level) {
+
+}
+
+void LevelRender(Level *level) {
+  for(int i = 0; i < level->brickCount; i++) {
+    if(level->bricks[i].isActive) {
+      Rectangle brick_rect = (Rectangle){ level->bricks[i].pos.x * level->brick_width,
+                                          level->bricks[i].pos.y * level->brick_height, 100, 50};
+      DrawRectangleRec(brick_rect, level->bricks[i].color);
+      DrawRectangleLines(brick_rect.x, brick_rect.y, brick_rect.width, brick_rect.height, BLACK);
+    }
+  }
 }
 
 // Read in the file and return a pointer to the buffer
@@ -80,10 +102,8 @@ static char *ReadLevelFile(const char *filename)
   // Copy all the text into the buffer
   fread(buffer, sizeof(char), numbytes, file_ptr);
 
-#ifdef DEBUG
   // Confirm we have read the file by outputing it to the console
-  printf("The file contains this text\n\n%s", buffer);
-#endif // DEBUG
+  // printf("The file contains this text\n\n%s", buffer);
 
   // Always close the file pointer when finished
   fclose(file_ptr);
@@ -93,15 +113,15 @@ static char *ReadLevelFile(const char *filename)
 
 static int GetLevel(Game *game, const char *const breakout_levels)
 {
-  const cJSON *level;
-  const cJSON *levels;
-  const cJSON *bricks;
-  const cJSON *brick;
-  int levelCount;
-  int brickCount;
+  const cJSON *level = NULL;
+  const cJSON *levels = NULL;
+  const cJSON *bricks = NULL;
+  const cJSON *brick = NULL;
+  int levelCount = 0;
+  int brickCount = 0;
   int index = 0;
-
   int status = 0;
+
   cJSON *root = cJSON_Parse(breakout_levels);
   if (root == NULL)
   {
@@ -110,8 +130,7 @@ static int GetLevel(Game *game, const char *const breakout_levels)
     {
       fprintf(stderr, "Error before: %s\n", error_ptr);
     }
-    status = 0;
-    goto end;
+    return status;
   }
 
   // Get "levels" array
@@ -119,6 +138,7 @@ static int GetLevel(Game *game, const char *const breakout_levels)
   levelCount = cJSON_GetArraySize(levels);
 
   // Find the current level
+  bool levelFound = false;
   cJSON_ArrayForEach(levels, levels)
   {
     level = cJSON_GetObjectItemCaseSensitive(levels, "level");
@@ -128,19 +148,33 @@ static int GetLevel(Game *game, const char *const breakout_levels)
       if (atoi(level->valuestring) == game->level->levelNumber)
       {
         printf("Level found!\n");
-        goto bricks;
+        levelFound = true;
+        break;
       }
     }
     index++;
   }
 
-bricks:
-  bricks = cJSON_GetArrayItem(levels, 1);
-  game->level->blockCount = cJSON_GetArraySize(bricks);
+  if (!levelFound)
+  {
+    cJSON_Delete(root);
+    return 0;
+  }
 
-  // game->level->blocks = 
+  bricks = cJSON_GetArrayItem(levels, 1);
+  // if (bricks == NULL || !cJSON_IsArray(bricks))
+  if (bricks == NULL)
+  {
+    cJSON_Delete(root);
+    return status;
+  }
+
+  game->level->brickCount = cJSON_GetArraySize(bricks);
+
+  game->level->bricks = (Brick*)malloc(game->level->brickCount * sizeof(Brick));
 
   // Get all bricks
+  Brick *current_brick = game->level->bricks;
   cJSON_ArrayForEach(bricks, bricks)
   {
     cJSON *row;
@@ -152,10 +186,39 @@ bricks:
     color = cJSON_GetObjectItemCaseSensitive(bricks, "color");
 
     printf("row %d, col %d, color %s\n", row->valueint, col->valueint, color->valuestring);
+    // current_brick = (Brick*)malloc(sizeof(Brick));
+    *current_brick = (Brick) {
+      .pos = (Vector2){ .x = (float)col->valueint, .y = (float)row->valueint },
+      .color = HexColorStringToColor(color->valuestring),
+      .health = 1,
+      .points = 100,
+      .isActive = true,
+    };
+
+    current_brick++;
   }
 
-end:
   cJSON_Delete(root);
 
   return status;
+}
+
+void ClearLevel(Level *level) {
+  if(level->bricks) {
+    free(level->bricks);
+  }
+  level->brickCount = 0;
+  level->columns = 0;
+  level->rows = 0;
+}
+
+static Color HexColorStringToColor(char *hex_str) {
+  Color color = (Color){ 0, 0, 0 , 0 };
+  unsigned int r, g, b;
+
+    if (hex_str && sscanf_s(hex_str, "#%02x%02x%02x", &r, &g, &b) == 3) {
+        color = (Color){ (unsigned char)r, (unsigned char)g, (unsigned char)b, 255 };
+    }
+
+  return color;
 }
